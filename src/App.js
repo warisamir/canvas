561f3779ca966b40f1d97b20ea58610a25a74e63
warisamir <warisamir1918@gmail.com>
2024-04-14 06:04:19 +0530

    addingsomechange

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CanvasEditor from './components/CanvasEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <CanvasEditor/>
+    </div>
+  );
+}
+
+export default App;
